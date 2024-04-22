8f45cc71e4e17792246b4b5442932facbc79e6d4
Tiques1 <tiques13@mail.ru>
2024-04-23 01:26:15 +0500

    end with parser functions

--- source/Parsing/YaMusicParser.py
+++ source/Parsing/YaMusicParser.py
@@ -27,7 +27,7 @@ class YaMusicParser:
         self.opt.add_experimental_option("prefs", prefs)
         self.opt.add_argument(r'--profile-directory=Default')
         self.opt.add_argument(r"--user-data-dir=C:\Users\Сергей\AppData\Local\Google\Chrome\User Data")
-        # self.opt.add_extension('..\\..\\recources\\HFOFHOFFDCFCJGMILKPNHKAMCGEMABAN_2_0_8_0.crx')
+        # self.opt.add_extension('..\\..\\resources\\HFOFHOFFDCFCJGMILKPNHKAMCGEMABAN_2_0_8_0.crx')
 
         self.browser = webdriver.Chrome(options=self.opt)
         self.browser.set_page_load_timeout(1000)
@@ -42,8 +42,8 @@ class YaMusicParser:
     def get_buttons(self):
         return self.browser.find_elements(By.ID, self.__download_button)
 
-    # Click button and download track into self.save_dir. Return album id, track id, track name
-    def download(self, button) -> (str, str, str):
+    # Click button and download track into self.save_dir. Return album id, track id
+    def download(self, button) -> (str, str):
         # Because button will updated and I need observe it
         parent = button.find_element(By.XPATH, "./..")
 
@@ -76,21 +76,66 @@ class YaMusicParser:
                 pass
 
         # Album id, Track id, Track name
-        return album.get_attribute('href').split('/')[4], album.get_attribute('href').split('/')[6], album.text
+        return album.get_attribute('href').split('/')[4], album.get_attribute('href').split('/')[6]
 
-    # Only on album's page
-    def get_artist(self):
-        artname = self.browser.find_elements(By.XPATH, "//h1[@class='page-artist__title typo-h1 typo-h1_big']")[0].text
-        artid = self.url.split('/')[4]
-        return artid, artname
+    # Only with link like /album/album_id/track/track_id
+    def get_track(self):
+        side_bar = self.browser.find_element(By.XPATH, "//div[@class='sidebar__placeholder sidebar__sticky']")
+        span = side_bar.find_element(By.XPATH, "//span[@class='d-artists']")
+        artist_list = span.find_elements(By.XPATH, "//a[@class='d-link deco-link']")
+        artists = []
+        for artist in artist_list:
+            artists.append(artist.get_attribute('href').split('/')[4])
+
+        name = self.browser.find_element(By.XPATH, "//div[@class='d-track__name']").get_attribute('title')
+
+        album_num = self.browser.find_element(By.XPATH, "//div[@class='d-track typo-track d-track_selectable"
+                                                        " d-track_inline-meta _music_ready']").get_attribute('data-id')
+        return artists, name, album_num
 
     # Only on album's page
     def get_album(self):
-        pass
+        cover = self.browser.find_element(By.XPATH, "//img[@class='entity-cover__image deco-pane']")\
+            .get_attribute('src')
+
+        information_div = self.browser.find_element(By.XPATH, "//div[@class='d-generic-page-head__main-top']")
+        name = information_div.find_element(By.XPATH, "//span[@class='deco-typo']").text
+        is_single = information_div.find_element(By.XPATH, "//span[@class='stamp__entity']").text
+        year = information_div.find_element(By.XPATH, "//span[@class='typo deco-typo-secondary']").text
+
+        genre = []
+        for g in information_div.find_elements(By.XPATH, "//a[@class='d-link deco-link deco-link_mimic typo']"):
+            genre.append(g.text)
+
+        artists = []
+        for a in information_div.find_elements(By.XPATH, "//a[@class='page-album__artists-short']"):
+            artists.append(a.get_attribute('href').split('/')[4])
+
+        label = []
+        label_div = self.browser.find_element(By.XPATH, "//div[@class='page-album__label']")
+        for lbl in label_div.find_elements(By.XPATH, "//a[@class='d-link deco-link']"):
+            label.append(lbl.get_attribute('href'))
+
+        return cover, name, is_single, year, genre, artists, label
+
+    # Only on artist page
+    def get_artist(self):
+        name = self.browser.find_element(By.XPATH, "//h1[@class='page-artist__title typo-h1 typo-h1_big']").text
+        try:
+            avatar = self.browser.find_element(By.XPATH, "//img[@class='artist-pics__pic']") \
+                .get_attribute('src')
+        except:
+            avatar = self.browser.find_element(By.XPATH, "//img[@class='artist-pics__pic artist-pics__pic_empty']") \
+                .get_attribute('src')
+
+        return name, avatar
 
     # Only on label's page
     def get_label(self):
-        pass
+        name = self.browser.find_element(By.XPATH, "//div[@class='page-label__title']")\
+            .find_element(By.TAG_NAME, "h1").text
+
+        return name
 
     @property
     def url(self):
@@ -125,11 +170,15 @@ def main():
         print('id альбома', alb, end='\n\n')
 
     # Get list of links from db
-    links = ('https://music.yandex.ru/album/20307442/track/98072491', )
+    links = ('https://music.yandex.ru/album/20448306/track/98462689', )
     for link in links:
         parser.url = link
         try:
             parser.browse()
+            tracks = parser.get_track()
+            for track in tracks:
+                # writing in db track information
+                pass
         except TimeoutException:
             return
 
