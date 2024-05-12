3c958fb9a2215bc446663c7648264449d59bba15
Tiques1 <tiques13@mail.ru>
2024-05-12 19:22:03 +0500

    music recommender

--- source/Analyzing/Music_recommendation.py
+++ source/Analyzing/Music_recommendation.py
@@ -5,61 +5,73 @@ import os
 import librosa
 import numpy as np
 from scipy.spatial.distance import cosine
-import scipy.stats
+# import scipy.stats
 
 
-def files(path):
-    for file in os.listdir(path):
-        if os.path.isfile(os.path.join(path, file)):
-            yield path + '\\' + file
-list_files = []
-for file in files("C:\\Users\\Leonochek\\Desktop\\music"):
-       list_files.append(file)
-print(list_files)
+class Files:
+    def __init__(self, path):
+        self.list_files = []
+        self.path = path
+
+        for file in self.files(self.path):
+            self.list_files.append(file)
+
+    @staticmethod
+    def files(path):
+        for f in os.listdir(path):
+            if os.path.isfile(os.path.join(path, f)):
+                yield path + '\\' + f
 
 
-def extract_features(audio_path):
-    y, sr = librosa.load(audio_path)
+class Extractor:
+    def __init__(self):
+        # (song_name, track_features)
+        self.couples_song_features = []
 
-    # Извлечение MFCC, хромограммы, спектрального центроида, мел-спектрограммы и оценки глобального темпа
-    onset_env = librosa.onset.onset_strength(y=y, sr=sr)
-    tempo = librosa.feature.tempo(onset_envelope=onset_env, sr=sr)
-    melspectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
-    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
-    chroma = librosa.feature.chroma_stft(y=y, sr=sr)
-    spectral_centroid = librosa.feature.spectral_centroid(y=y, sr=sr)
-    
-    # Создание признакового вектора путем объединения характеристик
-    features = [tempo[0], np.mean(melspectrogram), np.mean(mfccs), np.mean(chroma), np.mean(spectral_centroid)]
-    return features
+    @staticmethod
+    def extract_features(audio_path):
+        y, sr = librosa.load(audio_path)
 
-def create_songs_base(music_files):
-    for song in music_files:
-        audio_path = song
-        song_name = audio_path.split("\\")[-1][0:-4]
-        track_features = (extract_features(audio_path))
-        couple_song_features = (song_name, track_features)
-        couples_song_features.append(couple_song_features)
-    return couples_song_features
+        # Извлечение MFCC, хромограммы, спектрального центроида, мел-спектрограммы и оценки глобального темпа
+        onset_env = librosa.onset.onset_strength(y=y, sr=sr)
+        tempo = librosa.feature.tempo(onset_envelope=onset_env, sr=sr)
+        melspectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
+        mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+        chroma = librosa.feature.chroma_stft(y=y, sr=sr)
+        spectral_centroid = librosa.feature.spectral_centroid(y=y, sr=sr)
 
-#songs_base здесь - это список кортежей ("Название песни" - ["Объединенные характеристики в вектор"])
-def recommend_song(users_song, songs_base):
-        song_features = extract_features(users_song)
+        # Создание признакового вектора путем объединения характеристик
+        features = [tempo[0], np.mean(melspectrogram), np.mean(mfccs), np.mean(chroma), np.mean(spectral_centroid)]
+        return features
+
+    def create_songs_base(self, music_files):
+        for song in music_files:
+            song_name = song.split("\\")[-1][0:-4]
+            track_features = (self.extract_features(song))
+            self.couples_song_features.append((song_name, track_features))
+
+
+class Reccommender:
+    def __init__(self, extractor_):
+        self.extractor = extractor_
+        self.similar_tracks = []
+
+    def recommend_song(self, users_song, songs_base):
+        song_feature = self.extractor.extract_features(users_song)
         for track in songs_base:
-            similarity = 1 - cosine(song_features, track[1])
-            if similarity > 0.99992 and similarity < 1 :
-                similar_tracks.append(track[0])
-        print('Песни с такими характеристиками Вам понравятся')
-        return similar_tracks
-    
-similar_tracks = []
-features_of_songs = []    
-couples_song_features = []    
-
-title_and_data = create_songs_base(list_files)
-
-#Пример рекомендаций для одной из песен
-print(recommend_song(r"C:\Users\Leonochek\Desktop\music\Bring Me The Horizon - Sleepwalking.mp3",title_and_data))
+            similarity = 1 - cosine(song_feature, track[1])
+            if 0.99992 <= similarity <= 1:
+                self.similar_tracks.append((track[0], similarity))
 
 
+if __name__ == '__main__':
+    files = Files("D:\\Music")
 
+    extractor = Extractor()
+    extractor.create_songs_base(files.list_files)
+
+    recommender = Reccommender(extractor)
+    recommender.recommend_song(r"D:\Music\Paul Mauriat and His Orchestra,Вольфганг Амадей Моцарт - Symphony No. 40 In G Minor K550_ 1. Molto Allegro.mp3",
+                               extractor.couples_song_features)
+
+    print(sorted(recommender.similar_tracks, reverse=True, key=lambda x: x[1]))
--- source/Analyzing/diagrams.py
+++ source/Analyzing/diagrams.py
@@ -1,6 +1,33 @@
-class Diagram:
-    def __init__(self):
-        pass
+from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
 
-    def draw(self):
-        pass
+
+def kmeans(x_train_pca, kmeans=None):
+    if kmeans is None:
+        kmeans = KMeans(n_clusters=3, random_state=42)
+        clusters = kmeans.fit_predict(x_train_pca)
+    else:
+        clusters = kmeans.predict(x_train_pca)
+    return clusters, kmeans
+
+
+def tsne_plot(x_train_pca, clusters):
+    tsne = TSNE(random_state=42)
+    x_train_tsne = tsne.fit_transform(x_train_pca)
+
+    plt.figure(figsize=(10, 7))
+    scatter = plt.scatter(x_train_tsne[:, 0], x_train_tsne[:, 1], c=clusters, cmap='tab10', alpha=0.7)
+    plt.colorbar(scatter, label='Cluster')
+    plt.xlabel('t-SNE Component 1')
+    plt.ylabel('t-SNE Component 2')
+    plt.title('Clusters in t-SNE Space')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    x_train_pca =
+
+    clusters, kmeans = kmeans(x_train_pca)
+    tsne_plot(x_train_pca, clusters)
