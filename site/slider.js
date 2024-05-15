d1ded17bd5ef2e4f47493495f89c89efbdd52825
Tiques1 <tiques13@mail.ru>
2024-05-15 16:01:25 +0500

    site

---
+++ site/slider.js
@@ -0,0 +1,40 @@
+function sendRequest() {
+  // Путь к файлу на вашем локальном компьютере или сервере
+  const url = "D:\Python\Music-Analyzer\site\slider2.html"; // Например, 'data.php' если это PHP файл
+
+  // Данные для отправки на сервер
+  const data = {
+    slider1: document.getElementById('slider1').value,
+    slider2: document.getElementById('slider2').value,
+    slider3: document.getElementById('slider3').value,
+    slider4: document.getElementById('slider4').value,
+    slider5: document.getElementById('slider5').value
+  };
+
+  // Опции запроса
+  const options = {
+    method: 'POST', // Метод запроса
+    headers: {
+      'Content-Type': 'application/json' // Тип содержимого
+    },
+    body: JSON.stringify(data) // Преобразование данных в JSON формат
+  };
+
+  // Отправка запроса на сервер
+  fetch(url, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Преобразование ответа в JSON
+    })
+    .then(data => {
+      // Обработка полученных данных
+      console.log('Server response:', data);
+      // Возможно, выполнение каких-то действий с полученными данными
+    })
+    .catch(error => {
+      // Обработка ошибок
+      console.error('There was a problem with the fetch operation:', error);
+    });
+}
