f350dcd8940d50054e309daa2abc554288b9e6f5
Tiques1 <tiques13@mail.ru>
2024-05-27 21:11:52 +0500

    what i'm doing here?

--- source/UI/tracks/urls.py
+++ source/UI/tracks/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.tracks, name='home'),
-    path('<str:pk>/', views.TrackDetailView.as_view(), name='detail_track')
+    path('<int:id_>/', views.track, name='track')
 ]
--- source/UI/tracks/views.py
+++ source/UI/tracks/views.py
@@ -8,18 +8,9 @@ from DBHelper import DBHelper
 
 # Create your views here.
 
-
-def tracks(request):
-    # tracks = Track.objects.all()
-    # tracks = Track.objects.order_by('name')
-    name = request.GET.get('name')
-    # return render(request, "tracks/track.html", {'tracks': tracks})
-    db = DBHelper(database="music", user="postgres", password="1111", host='localhost')
-    db.exec(f'select * from track where name like {name}')
-    return render(request, "main/layout.html", {'tracks': db.fetch_many(10)})
+# Здесь выводится список похожих и прочая информация
+def track(request, id_):
 
 
-class TrackDetailView(DetailView):
-    model = Track
-    template_name = 'tracks/view.html'
-    context_object_name = 'track'
+    return render(request, "tracks/track.html")
+
